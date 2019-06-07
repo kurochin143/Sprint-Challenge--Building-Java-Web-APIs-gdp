c3b5e02425f4e28489874080d44a23777bc79cce
Israel dela Cruz <igdcruz42@gmail.com>
2019-06-07 11:00:14 -0500

    almost finished

--- sprint12/src/main/kotlin/com/israel/sprint12/CountryController.kt
+++ sprint12/src/main/kotlin/com/israel/sprint12/CountryController.kt
@@ -6,16 +6,21 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class CountryController {
 
+    companion object: Log()
+
     // /names
     @GetMapping("/names")
     fun namesAsc(): ResponseEntity<*> {
+        logger.info("/names accessed")
+
         val outCountryList = mutableListOf<Country>()
 
-        outCountryList.addAll(Sprint12Application.gdpList.data)
+        outCountryList.addAll(Sprint12Application.countryList.data)
 
         outCountryList.sortBy { it.name }
 
@@ -25,9 +30,11 @@ class CountryController {
     // /economy
     @GetMapping("/economy")
     fun economyDes(): ResponseEntity<*> {
+        logger.info("/economy accessed")
+
         val outCountryList = mutableListOf<Country>()
 
-        outCountryList.addAll(Sprint12Application.gdpList.data)
+        outCountryList.addAll(Sprint12Application.countryList.data)
 
         outCountryList.sortByDescending { it.gdp }
 
@@ -36,16 +43,52 @@ class CountryController {
 
     // /country/{id}
     @GetMapping("/country/{id}")
-    fun singleCountry(@PathVariable("id") id: Long) {
+    fun singleCountry(@PathVariable("id") id: Long): ResponseEntity<*> {
+        logger.info("/country/{id} accessed")
+
         var outCountry: Country? = null
 
-        Sprint12Application.gdpList.data.forEach {
+        Sprint12Application.countryList.data.forEach {
             if (it.id == id) {
                 outCountry = it
             }
         }
 
         outCountry ?: throw CountryNotFoundException(id)
+
+        return ResponseEntity(outCountry, HttpStatus.OK)
+    }
+
+    // /country/stats/median
+    @GetMapping("/country/stats/median")
+    fun getCountryWithMediaGDP(): ResponseEntity<*> {
+        logger.info("/country/stats/median accessed")
+
+        var countryListByGDP = mutableListOf<Country>()
+        countryListByGDP.addAll(Sprint12Application.countryList.data)
+
+        countryListByGDP.sortBy { it.gdp }
+
+        val outCountry = countryListByGDP[(countryListByGDP.size / 2) + 1]
+
+        return ResponseEntity(outCountry, HttpStatus.OK)
+    }
+
+    // /economy/table
+    @GetMapping("/economy/table")
+    fun displayCountryTableByEconomy(): ModelAndView {
+        logger.info("/economy/table accessed")
+
+        val mav = ModelAndView()
+        mav.viewName = "economyTable"
+        val countryListByEconomy = mutableListOf<Country>()
+        countryListByEconomy.addAll(Sprint12Application.countryList.data)
+
+        countryListByEconomy.sortByDescending { it.gdp }
+
+        mav.addObject("countryList", countryListByEconomy)
+
+        return mav
     }
 
 }
--- sprint12/src/main/kotlin/com/israel/sprint12/Sprint12Application.kt
+++ sprint12/src/main/kotlin/com/israel/sprint12/Sprint12Application.kt
@@ -2,15 +2,19 @@ package com.israel.sprint12
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.DispatcherServlet
 
 @SpringBootApplication
 class Sprint12Application {
 
     companion object {
-        val gdpList = CountryList()
+        val countryList = CountryList()
     }
 }
 
 fun main(args: Array<String>) {
-    runApplication<Sprint12Application>(*args)
+    val ctx = runApplication<Sprint12Application>(*args)
+
+    val dispatcherServlet = ctx.getBean("dispatcherServlet") as DispatcherServlet
+    dispatcherServlet.setThrowExceptionIfNoHandlerFound(true)
 }
